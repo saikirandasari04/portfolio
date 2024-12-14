67016ae0b8c1dbdf8585e2739f4579f7675a9d36
saikiran <saikiran.dasari04@gmail.com>
2024-12-14 15:22:22 +0530

    bug fixed

--- src/components/data-display/testimonial-details.tsx
+++ src/components/data-display/testimonial-details.tsx
@@ -13,7 +13,7 @@ const TestimonialDetails = ({
   return (
     <Card className="mx-auto flex flex-col items-center gap-6 p-8 md:w-2/3 md:p-12 lg:w-1/3">
       <Image src={personAvatar!} alt={`${personName} avatar`}></Image>
-      <Typography>&quot;{testimonial}&quot;</Typography>
+      <Typography>{testimonial}</Typography>
       <div className="flex w-full flex-col gap-1">
         <Typography
           variant="subtitle"
